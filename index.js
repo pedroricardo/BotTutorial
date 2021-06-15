b2655c490213ae7adb3382dc17230b85f1d988d0
PR <pedroricardorn@gmail.com>
2021-06-14 21:36:53 -0300

    aula 5

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+  
+
+client.on("ready", () => {
+  console.log("Estou online!!")  
+})
+
+client.on("message", (message) =>{ 
+
+    if(message.author.bot == true) return
+    if(message.channel.type == "dm") return
+
+
+
+    if(message.content == "ping"){
+      message.channel.send("Pong!")
+    }
+    if(message.content == "avatar"){
+      message.channel.send(`https://cdn.discordapp.com/avatars/518900443569717252/${message.author.avatar}.webp`)
+    }
+ 
+  })
+
+
+client.login('TOKEN')
+
