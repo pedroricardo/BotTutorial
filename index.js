f1e5bd9c018d64b8c4645183b6ae79c65db482e7
PR <pedroricardorn@gmail.com>
2025-07-11 18:16:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
   
 
 client.on("ready", () => {
-  console.log("Estou online!dedd")  
+  console.log("Estou online!deedd")  
 })
 
 client.on("message", (message) =>{ 
